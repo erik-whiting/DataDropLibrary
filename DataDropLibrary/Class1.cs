91b1171abb85c401f26ea6e2437dee8f9b56379a
erik-whiting <erik@erikwhiting.com>
2019-05-03 19:06:02 -0500

    Initial Commit

---
+++ DataDropLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDropLibrary
+{
+    public class Class1
+    {
+    }
+}
